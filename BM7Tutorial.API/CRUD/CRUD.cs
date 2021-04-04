fde7c6e111b1c592ab04ec448b0a093b9cf3806b
DhanarSantika <dhanar.santika@gmail.com>
2021-04-04 20:54:47 +0700

    Init Repo

---
+++ BM7Tutorial.API/CRUD/CRUD.cs
@@ -0,0 +1,102 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using BM7Tutorial.API.CRUD.DTO;
+using BM7Tutorial.DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace BM7Tutorial.API.CRUD
+{
+    public static class CRUD
+    {
+        [FunctionName("CRUD_GetAllClass")]
+        public static async Task<IActionResult> GetAll(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Class")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "cosmos-bl-tutorial-serverless")] DocumentClient documentClient,
+            ILogger log)
+        {
+            log.LogInformation("Triggering CRUD_GetAllClass by HTTP Trigger");
+
+            try
+            {
+                var query = new SqlQuerySpec("SELECT * FROM c");
+                var pk = new PartitionKey("Class/");
+                var options = new FeedOptions() { PartitionKey = pk };
+                var data = documentClient.CreateDocumentQuery(UriFactory.CreateDocumentCollectionUri("Course", "Class"), query, options);
+
+                return new OkObjectResult(data);
+            } catch (Exception e)
+            {
+                log.LogError($"Error : {e.Message}");
+
+                return new BadRequestObjectResult($"Error : {e.Message}");
+            }
+        }
+
+        [FunctionName("CRUD_GetClassById")]
+        public static async Task<IActionResult> GetAllById(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Class/{classId}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "Course",
+                collectionName: "Class",
+                ConnectionStringSetting = "cosmos-bl-tutorial-serverless",
+                Id = "{classId}",
+                PartitionKey = "Class/")] Class data,
+            ILogger log)
+        {
+            log.LogInformation("Triggering CRUD_GetClassById by HTTP Trigger");
+
+            try
+            {
+                return new OkObjectResult(data);
+            }
+            catch (Exception e)
+            {
+                log.LogError($"Error : {e.Message}");
+
+                return new BadRequestObjectResult($"Error : {e.Message}");
+            }
+        }
+
+        /*
+        [FunctionName("CRUD_CreateClass")]
+        public static async Task<IActionResult> CreateClass(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Class")] HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "cosmos-bl-tutorial")] DocumentClient documentClient,
+            ILogger log)
+        {
+            log.LogInformation("Triggering CRUD_GetClassById by HTTP Trigger");
+
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var data = JsonConvert.DeserializeObject<ClassDTO>(requestBody);
+
+                var classObj = new Class
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    PartitionKey = new PartitionKey("Class"),
+                    ClassCode = data.ClassCode,
+                    Description = data.Description
+                };
+
+                return new OkObjectResult(data);
+            }
+            catch (Exception e)
+            {
+                log.LogError($"Error : {e.Message}");
+
+                return new BadRequestObjectResult($"Error : {e.Message}");
+            }
+        }
+        */
+    }
+}
+
---
+++ BM7Tutorial.DAL/Model/Class.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Documents;
+using Newtonsoft.Json;
+using System;
+
+namespace BM7Tutorial.DAL
+{
+    public class Class
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("classCode")]
+        public string ClassCode { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
